390a183087e035b0d1c8a5bd82a1b8efec99f958
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2023-10-18 17:46:45 -0500

    Update PrimaryController.java

--- workspace/socketsTryServer/src/main/java/co/edu/uniquindio/programacionIII/socketsTryServer/controllers/PrimaryController.java
+++ workspace/socketsTryServer/src/main/java/co/edu/uniquindio/programacionIII/socketsTryServer/controllers/PrimaryController.java
@@ -8,59 +8,54 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class PrimaryController implements Runnable, Initializable {
-	//para que la clas este siempre a la escucha podemos implementar la interface de Runnable
-
+public class PrimaryController implements Initializable,Runnable {
     @FXML
     private Label lblTextos;
-    
-    //Se crea el hilo correspondiente
-    
+
+    private ServerSocket servidor;
+    private boolean serverRunning = true;
+
     @Override
-	public void initialize(URL location, ResourceBundle resources) {
-    	Platform.runLater(this);
-		
-	}
- 
+    public void initialize(URL location, ResourceBundle resources) {
+        // Inicia el servidor en un hilo separado para no bloquear la interfaz de usuario.
+        new Thread(this).start();
+    }
+
+    private void startServer() {
+        try {
+            servidor = new ServerSocket(9998);
+            Platform.runLater(() -> lblTextos.setText("Esperando mensajes..."));
+
+            while (serverRunning) {
+                Socket miSocket = servidor.accept();
+                DataInputStream flujoEntrada = new DataInputStream(miSocket.getInputStream());
+                String mensajeTexto = flujoEntrada.readUTF();
+                Platform.runLater(() -> lblTextos.setText(mensajeTexto));
+                miSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopServer() {
+        serverRunning = false;
+        try {
+            if (servidor != null) {
+                servidor.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 	@Override
 	public void run() {
-		//Podemos probar este Hilo con un syso y con solo cargar la aplicacion deberia de implimir algo en consola
-		//System.out.println("Oeeee");
-		
-		//Una vez comprobado el hilo podemos ya pasar a la escucha con el puesto necesario
-		
-		try {
-			ServerSocket servidor= new ServerSocket(9998);
-			System.out.println("conexion hecha");
-			//Ahora lo que hay que hacer es decirle que acepte cualquier conexion que le llegue del exterior
-			Socket miSocket= servidor.accept();
-			System.out.println("socket aceptado");
-			//Ahora debemos hacer un flujo de entratda con el socked que se acaba de recibir
-			DataInputStream flujoEntrada= new DataInputStream(miSocket.getInputStream());
-			System.out.println("flujo generado");
-			//Ahora debemos saber leer que nos llega en el flujo de entrada
-			String mensajeTexto= flujoEntrada.readUTF();
-			System.out.println("texto que llego: "+mensajeTexto);
-			//Y ahora se procede a escribirse en su el textArea
-			lblTextos.setText(mensajeTexto);
-			//cerramos la conexion
-			miSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		startServer();		
 	}
-
-	
-    
-    
-    
-    
-
 }
+
